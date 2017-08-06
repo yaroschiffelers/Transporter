c896a2c7206a862d499fac14c1ea86b952b9e24b
Yaro Schiffelers <yaroschiffelers@gmail.com>
2017-08-06 16:31:04 +0200

    v1.1
    Completed version 1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Transporter
+Put the programs you want to download in mylist.txt 
+Please make shure mylist.txt isn't empty, otherwise the script will keep looping 
+Run the script 
+
+It'll tell you which programs are downloadable via brew cask and download them for you
+
+## TODO: 
+- complete README.md 
+- add brew/brewcask installer 
+- find workaround for empty mylist.txt
---
+++ transporter.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Checks if the programs listed in mylist.txt are downloadable via brew cask. Downloads them if true. 
+# And some other stuff, need to complete readme.md. 
+# Version 1.1
+# 06-08-2017
+# Yaro Schiffelers
+# github.com/yaroschiffelers
+
+# SCRIPT
+# Get current list of downloadable programs via brew cask
+brew cask search >| fullcasklist.txt
+
+# Convert mylist.txt to mylist with only lowercase letters 
+tr '[:upper:]' '[:lower:]' < mylist.txt >| mylistlowercase.txt
+
+# Import mylistlowercase.txt to arrayA 
+IFS=$'\n' read -d '' -r -a arrayA < mylistlowercase.txt
+
+# Compare mylistlowercase.txt to fullcasklist.txt Output the results to matches.txt
+grep -x ${arrayA[@]/#/-e } fullcasklist.txt >| matches.txt
+
+echo "######################"
+echo "Programms to download:"
+
+# Import matches.txt to arrayB
+IFS=$'\n' read -d '' -r -a arrayB < matches.txt
+
+arrayBlength=${#arrayB[@]}
+
+# Show user the programs cask can download for them 
+for (( i=1; i<${arrayBlength}+1; i++ ));
+do
+ 	echo ${arrayB[$i-1]}
+done
+
+# Ask user whether or not they want to download these programs 
+echo "################################"
+echo -n -e "Do you want to download these programms (y/n)?\n"
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg  
+
+	if echo "$answer" | grep -iq "^y" ;then
+
+		for (( i=1; i<${arrayBlength}+1; i++ ));
+		do 
+			brew cask install ${arrayB[$i-1]}
+			echo "Exit"
+		done
+
+	else 
+		echo "Exit"
+
+	fi
