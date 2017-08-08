246c6675182b1e61452fa31d488f5634ee6796ef
Yaro Schiffelers <yaroschiffelers@gmail.com>
2017-08-08 16:35:41 +0200

    Edit
    - edit info
    - added some $echo statements

--- transporter.sh
+++ transporter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Checks if the programs listed in mylist.txt are downloadable via brew cask. Downloads them if true. 
-# And some other stuff, need to complete readme.md. 
-# Version 1.1
-# 06-08-2017
+# Checks if the programs listed in mylist.txt are downloadable via brew cask. 
+# Downloads the programs that can be downloaded. 
+# Need to complete readme.md
+# Version 1.2
+# 08-08-2017
 # Yaro Schiffelers
 # github.com/yaroschiffelers
 
@@ -21,17 +22,15 @@ brew cask search >| fullcasklist.txt
 echo "Converting mylist.txt"
 mylistlowercase=($(tr '[:upper:]' '[:lower:]' < mylist.txt))
 
-# Compare $mylistlowercase[] to $fullcasklist[]. Output the results to matches[]
+# Compare $mylistlowercase[] to fullcasklist.txt Output the results to matches.txt
+echo "Comparing"
 grep -x ${mylistlowercase[@]/#/-e } fullcasklist.txt >| matches.txt
 IFS=$'\n' read -d '' -r -a matches < matches.txt
 
+# Show the user the programs cask can download for them 
 echo "######################"
 echo "Programms to download:"
-
-# Import matches.txt to arrayB
 matcheslength=${#matches[@]}
-
-# Show user the programs cask can download for them 
 for (( i=1; i<${matcheslength}+1; i++ ));
 do
  	echo ${matches[$i-1]}
@@ -43,14 +42,16 @@ echo -n -e "Do you want to download these programms (y/n)?\n"
 old_stty_cfg=$(stty -g)
 stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg  
-
+stty $old_stty_cfg
+	# Download the programs in $matches[] via brew cast install
 	if echo "$answer" | grep -iq "^y" ;then
 		for (( i=1; i<${matcheslength}+1; i++ ));
 		do 
 			brew cask install ${matches[$i-1]}
 			echo "Exit"
+			exit
 		done
 	else 
 		echo "Exit"
+		exit
 	fi
